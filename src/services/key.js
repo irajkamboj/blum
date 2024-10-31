a8bd2044987c587926c50c52fada57df9f7913c6
Raj kamboj <37168409+irajkamboj@users.noreply.github.com>
2024-11-01 01:03:00 +0530

    Update key.js

--- src/services/key.js
+++ src/services/key.js
@@ -1,53 +1,29 @@
-import axios from "axios";
 import colors from "colors";
 import inquirer from "inquirer";
 import fileHelper from "../helpers/file.js";
 import gameService from "./game.js";
-import server from "./server.js";
 
 class KeyService {
   constructor() {}
 
   maskApiKey(apiKey) {
-    // Split the string into 3 parts: the beginning, the middle to mask, and the end
     const parts = apiKey.split("_");
     if (parts.length !== 2) {
       throw new Error("Invalid API key format");
     }
 
-    const prefix = parts[0]; // 'pro'
-    const key = parts[1]; // 'a8ff5ce14b57853563c44988a890dca2'
+    const prefix = parts[0];
+    const key = parts[1];
 
-    // Take the first 6 characters and the last 4 characters of the key
     const start = key.slice(0, 6);
     const end = key.slice(-6);
 
-    // The middle part will be replaced with '*' characters
     const maskedMiddle = "*".repeat(key.length - start.length - end.length);
 
-    // Combine the masked string back
     return `${prefix}_${start}${maskedMiddle}${end}`;
   }
 
-  async checkKey(database, apiKey) {
-    try {
-      const URL = database?.server?.pro[0].url;
-      const endpoint = `${URL}blum/check-limit`;
-
-      const { data } = await axios.get(endpoint, {
-        headers: {
-          "X-API-KEY": apiKey,
-        },
-      });
-      return data;
-    } catch (error) {
-      return null;
-    }
-  }
-
   async handleApiKey() {
-    const database = await server.getData();
-
     const rawKeys = fileHelper.readFile("key.txt");
     const keys = rawKeys
       .split("\n")
@@ -56,24 +32,8 @@ class KeyService {
 
     if (keys.length) {
       const apiKey = keys[0];
-
-      const check = await this.checkKey(database, apiKey);
-      if (check === null) {
-        console.log(
-          colors.red(
-            `Invalid API KEY, contact Telegram @zuydd to get/purchase an API KEY`
-          )
-        );
-      } else {
-        gameService.setApiKey(apiKey);
-        gameService.setQuota(check?.data);
-        const maskedKey = this.maskApiKey(apiKey);
-        console.log(
-          `API KEY: ${colors.green(maskedKey)} - Remaining ${colors.green(
-            check?.data
-          )} uses`
-        );
-      }
+      gameService.setApiKey(apiKey);
+      console.log(`API KEY: ${colors.green(this.maskApiKey(apiKey))}`);
     } else {
       const response = await inquirer.prompt([
         {
@@ -85,24 +45,9 @@ class KeyService {
       ]);
       const { apiKey } = response;
       if (apiKey) {
-        const check = await this.checkKey(database, apiKey);
-        if (check === null) {
-          console.log(
-            colors.red(
-              `Invalid API KEY, contact Telegram @zuydd to get/purchase an API KEY`
-            )
-          );
-        } else {
-          fileHelper.writeLog("key.txt", apiKey);
-          gameService.setApiKey(apiKey);
-          gameService.setQuota(check?.data);
-          const maskedKey = this.maskApiKey(apiKey);
-          console.log(
-            `API KEY: ${colors.green(maskedKey)} - Remaining ${colors.green(
-              check?.data
-            )} uses`
-          );
-        }
+        fileHelper.writeLog("key.txt", apiKey);
+        gameService.setApiKey(apiKey);
+        console.log(`API KEY: ${colors.green(this.maskApiKey(apiKey))}`);
       }
     }
   }
